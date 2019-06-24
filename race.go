8ad62685fb97729fbd5bce08d89c5084a8c4b981
bkellogg7 <brk7vu@virginia.edu>
2019-06-24 14:10:22 -0400

    pulled some code FindShortestWikiPath to visit Node and added comments

--- race.go
+++ race.go
@@ -15,8 +15,10 @@ type node struct {
   parent *node
 }
 
+//Slice of prefixes of wikipedia pages to ignore in order to only get pages of actual articles
 var prefixes = [...]string{"/wiki/Main_Page", "/wiki/Special","/wiki/File","/wiki/Help","/wiki/Wikipedia:"}
 
+/* checkListOfPrefixes: Checks url against slice of prefixes to ensure the linke is to an article.*/
 func checkListOfPrefixes(href string)bool{
   for _, prefix := range prefixes{
     if strings.HasPrefix(href,prefix){
@@ -26,6 +28,8 @@ func checkListOfPrefixes(href string)bool{
   return false
 }
 
+/* processElement: Checks if url is valid for the race and formats it to the full url
+                   to prepare it for a get request.*/
 func processElement(index int, element *goquery.Selection)(string) {
   // See if the href attribute exists on the element
   href, exists := element.Attr("href")
@@ -35,28 +39,40 @@ func processElement(index int, element *goquery.Selection)(string) {
   return ""
 }
 
+/* getNewLinks: Ansynchronous function that performs a get request to a url to
+                ensure it is valid, creates a slice of all the valid urls from
+                the page for the race, returns the slice in the provided channel. */
 func getNewLinks(a node, out chan<- []node,path_num int){
+  //slic to add new links to
   q := make([]node,0)
+
+  //Get request for the url
   resp, err := http.Get(a.url)
   if err != nil{
     log.Print("Error getting page", err)
     out <- q
     return
   }
+
   document, queryError := goquery.NewDocumentFromReader(resp.Body)
   if queryError != nil {
      log.Fatal("Error loading HTTP response body. ", queryError)
   }
+
+  //find all links within the body of the page
   links := document.Find("a").Map(processElement)
   for _, link := range links{
       if link != ""{
         q = append(q, node{url:link,path:path_num,parent:&a})
       }
   }
+
+  //send slice to the channel
   out <- q
 }
 
-
+/*getPath: concatenate the path from node1 to the starting node with the path of
+           node2 to the ending node.*/
 func getPath(node1 *node, node2 *node)string{
   path := ""
   for node1 != nil{
@@ -72,7 +88,37 @@ func getPath(node1 *node, node2 *node)string{
   return path
 }
 
+
+func VisitNode(m map[string]node, q []node, threadCount int,  out chan<- []node, path int, paths []string)(map[string]node, []node, int, []string){
+  if len(q) > 0{
+    var a node
+    var val node
+    var found bool
+
+    //pop a url from the queue
+    a, q = q[0], q[1:]
+    //if the url is already in the map and it was not found in this half
+    //of the search, then path has been found from start to finish
+    if val, found = m[a.url]; found && val.path != path {
+     paths = append(paths, getPath(&a, &val))
+    }
+    if !found {
+      //add url to map
+      m[a.url] = a
+      threadCount += 1
+      //asynch thread to get new links on page
+      go getNewLinks(a, out, path)
+    }
+  }
+  return m, q, threadCount, paths
+}
+
+/* FindShortestWikiPath: Returns a string indicating the shortest path between
+                         two wikipeida articles. Utilizes a BFS that starts from
+                         both the start and finish articles.*/
 func FindShortestWikiPath(article1 string, article2 string)(string, string){
+
+  //convert the proviced article names to wikipedia url's
   start := node {
                         url : "https://en.m.wikipedia.org/wiki/" + strings.Replace(article1, " ", "_", -1),
                         path: 1,
@@ -84,62 +130,50 @@ func FindShortestWikiPath(article1 string, article2 string)(string, string){
                         parent : nil,
                       }
 
+   //map from a url string to node representing a webpage that has been visited
    m := make(map[string]node)
+
+   //preliminary queues for the next round of BFS
    next_q1 := make([]node, 1)
    next_q1[0] = start
    next_q2 := make([]node,1)
    next_q2[0] = end
 
-   var a1 node
-   var a2 node
+   //current queue of url's to be visited in the BFS
    q1 := make([]node, 0)
    q2 := make([]node, 0)
 
+   //for each round of bfs
    for len(next_q1) > 0 || len(next_q2) > 0{
       q1 = next_q1
       q2 = next_q2
+
       next_q1 = make([]node, 0)
       next_q2 = make([]node, 0)
+
+      //contains all valid paths from start to finish found in a round
       paths := make([]string, 0)
 
+      //chnnels for each queue to return new links found on each page
       out1 := make(chan []node)
-      out1Count := 0
       out2 := make(chan []node)
+
+      //number of threads for each channel
+      out1Count := 0
       out2Count := 0
 
-      //fmt.Println(len(q1))
-      //fmt.Println(len(q2))
-
+      //for each element in the queues in this round of BFS
       for len(q1) > 0 || len(q2) > 0{
-        var val node
-        var found bool
 
-        if len(q1) > 0{
-           a1, q1 = q1[0], q1[1:]
-           if val, found = m[a1.url]; found && val.path != 1 {
-             paths = append(paths, getPath(&a1, &val))
-            }
-            if !found {
-              m[a1.url] = a1
-              out1Count += 1
-              go getNewLinks(a1, out1, 1)
-            }
-        }
+        m, q1, out1Count, paths = VisitNode(m, q1, out1Count, out1, 1, paths)
 
-        if len(q2) > 0{
-          a2, q2 = q2[0], q2[1:]
-          if val, found = m[a2.url]; found && val.path != 2 {
-             paths = append(paths, getPath(&val, &a2))
-           }
-          if !found{
-           m[a2.url] = a2
-           out2Count +=1
-           go getNewLinks(a2, out2, 2)
-          }
-        }
+        m, q2, out2Count, paths = VisitNode(m, q2, out2Count, out2, 2, paths)
+
        }
-       // fmt.Println("/////////////////////////////////////////////////////////////////////////////")
+
+       //If a valid path from starting node to finishing node is found
        if len(paths) != 0 {
+         //find the shortest path in the list of paths
          shortestLength := len(paths[0])
          index := 0
          for i, path := range paths {
@@ -150,10 +184,12 @@ func FindShortestWikiPath(article1 string, article2 string)(string, string){
          }
          return paths[index], ""
        }
+
+       //For each thread in ech channel, get the result from the thread
+       //and add it to next queue
        for i := 0; i < out1Count; i++ {
          next_q1 = append(next_q1, <-out1...)
-      }
-
+       }
        for i := 0; i < out2Count; i++ {
          next_q2 = append(next_q2, <-out2...)
        }
